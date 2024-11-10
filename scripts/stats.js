dc4c15308cf016c044507e3e81a176b512383693
rurkk <367414@niuitmo.ru>
2024-11-10 18:42:18 +0300

    +

--- scripts/stats.js
+++ scripts/stats.js
@@ -1,16 +1,11 @@
 (function() {
     window.addEventListener('load', function() {
-        // Получаем время загрузки с использованием performance.now()
-        const loadTime = performance.now();  // Возвращает время с момента начала загрузки страницы
-
-        // Находим элемент для вывода времени загрузки
+        const loadTime = performance.now();
         const loadTimeElement = document.getElementById('load-time');
         
-        // Создаем текст для вывода
-        const statsText = `Время загрузки страницы: ${loadTime.toFixed(0)} мс`; // Округляем до целых
+        const statsText = `Время загрузки страницы: ${loadTime.toFixed(0)} мс`;
 
-        // Добавляем текст в элемент
-        loadTimeElement.textContent = statsText; // Выводим результат в контейнер
+        loadTimeElement.textContent = statsText;
     });
 })();
 
